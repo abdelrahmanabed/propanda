4adecd6d7dc9eb01eadbdcc86da29bb15af4e762
abdelrahmanabed <107738342+abdelrahmanabed@users.noreply.github.com>
2024-05-24 04:55:59 +0300

    Update deletefromcartIcon.js

--- app/components/deletefromcartIcon.js
+++ app/components/deletefromcartIcon.js
@@ -4,21 +4,15 @@ import { Player } from '@lordicon/react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import CryptoJS from 'crypto-js';
-import TrashIcon from './trashicon';
+import { RiDeleteBin6Line } from "react-icons/ri";
 import { useCart } from './CartContext';
 const DeletefromcartIcon = (props) => {
     const playerRef = useRef(null);
-    const [state, setState] = useState("hover-empty");
-    const [added, setAdded] = useState('./wired-outline-185-trash-bin.json');
     const [showDMessage, setShowDMessage] = useState(false);
 
 const {removeFromCart} = useCart()
 
     
-    const handlePFB = () => {
-        // Play the animation when the mouse enters
-        playerRef.current?.playFromBeginning();
-    };
 
 
       
@@ -45,24 +39,19 @@ const {removeFromCart} = useCart()
                             },
                         });
                         
-                        console.log('Item removed from cartItems array');
-                        setState("morph-trash-in");
                         setShowDMessage(true);
                         setTimeout(() => {
                             setShowDMessage(false);
                         }, 1000);
-                        handlePFB();
                         removeFromCart(props.courseId) // Callback to parent component
 
             } 
                 }
             } else{
-                setState("morph-trash-in");
                 setShowDMessage(true);
                 setTimeout(() => {
                     setShowDMessage(false);
                 }, 1000);
-                handlePFB();
                 removeFromCart(props.courseId) // Callback to parent component
 
             }
@@ -70,27 +59,21 @@ const {removeFromCart} = useCart()
             console.error('Error:', error);
         }
     };
-    const ICON = require(`${added}`); // Replace with your actual icon data
 
     return (
         <>
         <div id='lordcdiv' 
             onClick={handleCartClick}
             className={`
-                ${state === 'in-oscillate' ? 'h-16 w-16 rounded-2xl' : 'clicked'}
-                duration-300 flex justify-center w-8 h-8 items-center  ${ props.className}   `}
+ rounded-2xl clicked duration-300 flex justify-center w-8 h-8 items-center  ${ props.className}   `}
         >
-            <Player
-                state={state}
-                size={30}
-                ref={playerRef}
-                icon={ICON}
-                colorize={"#000"}
+            <RiDeleteBin6Line
+               
                 currentState={props.currentState}
             />
 
         </div>
-        {showDMessage &&  <div className="messageyes flex flex-col justify-center items-center duration-500 fixed p-3 text-xl md:text-3xl rounded-2xl "><TrashIcon state="morph-trash-in"/> <span >تم ازالته من السلة</span></div>}
+        {showDMessage &&  <div className="messageyes flex flex-col justify-center items-center duration-500 fixed p-3 text-xl md:text-3xl rounded-2xl "><RiDeleteBin6Line /> <span >تم ازالته من السلة</span></div>}
 
                                 </>
     );
