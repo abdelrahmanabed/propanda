4d5c730b184eb953283e2c0e817c9ab5cfa18d94
abdelrahmanabed <107738342+abdelrahmanabed@users.noreply.github.com>
2024-05-24 19:00:04 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,15 +1,12 @@
 import { Suspense } from "react";
 import Hero from "./components/hero";
-import NewCourseContainer from "./components/NewCourseContainer";
 
 export default function Home() {
   return (
     <main className="">
       <div className="">
       <Hero/>
-      <Suspense fallback={<div>loading</div>}>
-        <NewCourseContainer/>
-      </Suspense>
+      
       </div>
     </main>
   );
