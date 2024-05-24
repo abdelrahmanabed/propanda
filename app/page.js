154ff5a8219368b3aca27cb7b1dfffeec915a965
abdelrahmanabed <107738342+abdelrahmanabed@users.noreply.github.com>
2024-05-24 06:25:02 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,10 @@
 import Hero from "./components/hero";
-import NewCourseContainer from "./components/NewCourseContainer";
 
 export default function Home() {
   return (
     <main className="">
       <div className="">
       <Hero/>
-        <NewCourseContainer/>
         
       
       </div>
