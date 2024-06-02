da46dd08a8ae879bb449f2dfb990b6e846fabbb9
abdelrahmanabed <107738342+abdelrahmanabed@users.noreply.github.com>
2024-06-03 01:05:56 +0300

    Update instructorCard.js

--- app/components/instructorCard.js
+++ app/components/instructorCard.js
@@ -5,8 +5,8 @@ const InstructorCard = (props) => {
   return (
     <div className=''>
         <Link href={props.href} className=' insCard gap-3 items-center  w-72 h-96 flex flex-col   rounded-2xl overflow-hidden'>
-    <div className=' w-full h-72 rounded-2xl flex justify-center items-start overflow-hidden'>
-    <Image src={`${process.env.NEXT_PUBLIC_PORT}/${props.image.replace(/\\/g, '/')}`} width={1024} height={1024} className='  min-h-full h-auto min-w-full w-auto  '/>
+    <div className=' w-full h-72 rounded-2xl flex justify-center items-center overflow-hidden'>
+    <Image src={`${process.env.NEXT_PUBLIC_PORT}/${props.image.replace(/\\/g, '/')}`} width={500} height={500} className='  min-h-full  min-w-full  '/>
     </div>
     <div className=' flex flex-col gap-3 items-center '>
             <span className=' font-bold'>{props.name}</span>
