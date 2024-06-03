2234e8db5f12c8e3b4cce72b181fd731e0b98f74
abdelrahmanabed <107738342+abdelrahmanabed@users.noreply.github.com>
2024-06-03 08:33:50 +0300

    Update NewCourseContainer.js

--- app/components/NewCourseContainer.js
+++ app/components/NewCourseContainer.js
@@ -1,41 +1,68 @@
+'use client'
 import "keen-slider/keen-slider.min.css"
 import dynamic from 'next/dynamic';
 import { Suspense } from "react";
 import Loading from "./loading";
 const Course = dynamic(() => import("./course"), { ssr: false });
-const Keenslider = dynamic(() => import("./Keenslider"), { ssr: false });
-
+import Keenslider from './Keenslider'
+import {  useState, useEffect } from "react";
 
 
 const NewCourseContainer = async () => {
-  
-        const response = await fetch(`${process.env.NEXT_PUBLIC_PORT}/api/courses`);
-        const newCourses = await response.json()
-     
-    return (
-      <Suspense fallback={<div className=" h-96 bg-white"><Loading/></div>}>
-        <div className=" mt-10 mx-3 min-h-96  mb-3 flex flex-col gap-3  overflow-hidden ">
-          <span className=" text-lg ">احدث الدورات التعليمية</span>
-          { newCourses&& newCourses.length > 0 &&  
-       <Keenslider>
-        { newCourses.map((course) => (
-          <div key={course._id} style={{ maxWidth: "fit-content", minWidth:"fit-content" }}
-          className="keen-slider__slide min-w-fit">
-            <Course
-              href={`/courses/${course._id}`}
-              photo={course.photo}
-              title={course.title}
-              price={course.price}
-              courseId={course._id}
-              instructor={course.author}
-            />
-          </div>
-        ))}
-     </Keenslider> }
-       
+  const [popularCourses, setPopularCourses] = useState([]);
+  const [displayedCourses, setDisplayedCourses] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-      </div></Suspense>
-    );
+  useEffect(() => {
+    const fetchCourses = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_PORT}/api/popularCourses?page=1&limit=14`);
+        const data = await response.json();
+        setPopularCourses(data);
+        setDisplayedCourses(data.slice(0, 7));
+      } catch (error) {
+        console.error("Failed to fetch courses:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCourses();
+  }, [displayedCourses]);
+
+  const handleSeeMore = () => {
+    const newCoursesToDisplay = popularCourses.slice(displayedCourses.length, displayedCourses.length + 7);
+    setDisplayedCourses((prevCourses) => [...prevCourses, ...newCoursesToDisplay]);
+  };
+
+  return (
+    <Suspense fallback={<div className="h-96 bg-white"><Loading /></div>}>
+      <div className="mt-10 mx-3 min-h-96 mb-3 flex flex-col gap-3 overflow-hidden">
+        <span className="text-lg">اكثر الدورات شعبية</span>
+        {displayedCourses && displayedCourses.length > 0 && (
+          <Keenslider>
+            {displayedCourses.map((course) => (
+              <div key={course._id} style={{ maxWidth: "fit-content", minWidth: "fit-content" }} className="keen-slider__slide min-w-fit">
+                <Course
+                  href={`/courses/${course._id}`}
+                  photo={course.photo}
+                  title={course.title}
+                  price={course.price}
+                  courseId={course._id}
+                  instructor={course.author}
+                />
+              </div>
+            ))}
+            {displayedCourses.length < popularCourses.length && (
+              <div style={{ maxWidth: "fit-content", minWidth: "fit-content" }} className="keen-slider__slide min-w-fit">
+                <button onClick={handleSeeMore} className="see-more-button">See More</button>
+              </div>
+            )}
+          </Keenslider>
+        )}
+      </div>
+    </Suspense>
+  );
 };
-
 export default NewCourseContainer;
