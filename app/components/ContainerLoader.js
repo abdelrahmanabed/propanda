f24198e449d7cc7e0eba6d0f909957fad0e0ffdb
abdelrahmanabed <107738342+abdelrahmanabed@users.noreply.github.com>
2024-06-11 07:24:14 +0300

    o

--- app/components/ContainerLoader.js
+++ app/components/ContainerLoader.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContainerLoad = () => {
   return (
-    <div className='ccontainer flex  gap-3 flex-col placeholder-item p-3'>
+    <div className='ccontainer flex m-3  gap-3 flex-col placeholder-item p-3'>
       <div className=' rounded-xl h-14 bg-zinc-300 w-full'></div>
       <div className=' rounded-xl h-14 bg-zinc-300 w-full'></div>
       <div className=' rounded-xl h-14 bg-zinc-300 w-full'></div>
